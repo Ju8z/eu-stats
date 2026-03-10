cb09f84a4041d58480b72b46ac7a6a083935f3b0
jubz <info@jubz.dev>
2026-03-10 19:14:44 +0100

    Update readme and demo localization

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
         }
     },
     server: {
+        host: '0.0.0.0',
+        port: 5173
+    },
+    preview: {
+        host: '0.0.0.0',
         port: 5173
     }
 });
